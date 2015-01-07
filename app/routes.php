7b535667e87d8510ac4f7924d0000c7d91016e30
Vincent Cardillo <vcardillo@gmail.com>
2015-01-06 22:54:23 -0800

    Database work.

--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,6 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
+
+
+#Route::controller('menu', 'MenuCtrl');
--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,10 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('homestead'),
+	'local' => array(
+            'homestead', 
+            'jupiter',
+        ),
 
 ));
 
