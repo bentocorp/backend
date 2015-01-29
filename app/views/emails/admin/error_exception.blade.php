9342e30ffd53aa1aa45e80dc663bee279ca1d478
Vincent Cardillo <vcardillo@gmail.com>
2015-01-29 11:39:26 -0500

    Adding  and  info to exception emails.

--- app/views/emails/admin/error_exception.blade.php
+++ app/views/emails/admin/error_exception.blade.php
@@ -20,6 +20,18 @@ var_dump ($user);
 echo "</pre>";
 
 
+echo '<h2>$_SERVER:</h2>';
+echo "<pre>";
+var_dump ($_SERVER);
+echo "</pre>";
+
+
+echo '<h2>$_REQUEST:</h2>';
+echo "<pre>";
+var_dump ($_REQUEST);
+echo "</pre>";
+
+
 ?>
   
   
