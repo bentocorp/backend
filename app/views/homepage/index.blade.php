28006b7d90803fe78082185aaf97e7a1a92266ef
Vincent Cardillo <vcardillo@gmail.com>
2015-03-02 11:52:16 -0800

    Adding site.

---
+++ app/views/homepage/index.blade.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>Bento — Delicious Asian food, in minutes, on-demand</title>
+    <meta name="description" content="Bento — Delicious Asian food, in minutes, on-demand">
+    <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;" />
+
+    <!-- <link rel="icon" type="image/png" href="favicon.png"> -->
+    <link rel="stylesheet" href="//d3q6uu7asevdsg.cloudfront.net/1.2/css/link_texting.min.css">
+    <link rel="stylesheet" href="/site/css/bootstrap.css">
+    <link href='//fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,700italic,700,600,400' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+    <section class="page-hero">
+        <nav class="navbar navbar-default navbar-home">
+            <div class="container">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="#">Download Now</a></li>
+                    <li class="twitter"><a href="https://twitter.com/share" class="twitter-share-button" data-via="sortino">Tweet</a></li>
+                    <li class="facebook"><div class="fb-like" data-href="https://developers.facebook.com/docs/plugins/" data-layout="standard" data-action="like" data-show-faces="true" data-share="false"></div></li>
+                </ul>
+            </div>
+        </nav>
+        <div class="hero hero-home">
+            <div class="container">
+                <div class="hero-left">
+                    <img src="/site/img/bento-logo.png" alt="Bento Logo" class="logo">
+                    <h1>Delicious Asian food delivered in minutes. <br/>
+                    Build your Bento for only $12.
+                    </h1>
+                    <div class="app-store-btn">
+                        <a href="#" class="btn btn-default btn-download">Download in App Store</a>
+                    </div>
+                    <form class="form-phone">
+                        <div class="linkTexting_oq3j39q0">
+                          <div class="linkTextingInner_oq3j39q0">
+                            <div class="input-group">
+                                <input class="linkTextingInput_oq3j39q0 form-control bfh-phone span1" type="tel" id="numberToText_linkTexting_oq3j39q0_5e87fc24-49d5-4321-8dd7-b239e516b843" data-format="+1 (ddd) ddd-dddd"></input>
+                                <span class="input-group-btn">
+                                    <button class="linkTextingButton_oq3j39q0 btn" type="button" onclick="sendLink_linkTexting_oq3j39q0('5e87fc24-49d5-4321-8dd7-b239e516b843')" id="sendButton_linkTexting_oq3j39q0_5e87fc24-49d5-4321-8dd7-b239e516b843">Text download link</button>
+                                </span>
+                            </div>
+                            <div class="linkTextingError_oq3j39q0" id="linkTextingError_oq3j39q0_5e87fc24-49d5-4321-8dd7-b239e516b843" style="display:none;"></div>
+                          </div>
+                        </div>
+                    </form>
+                    <script type="text/javascript" src="//d3q6uu7asevdsg.cloudfront.net/1.2/js/link_texting.min.js"></script>
+                </div>
+                <div class="hero-right">
+                    <div class="bento-screenshots">
+                        <img src="/site/img/bento-screenshots.png" alt="Screenshots of Bento iPhone App">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="steps">
+        <div class="container">
+            <div class="row steps-list">
+                <div class="col-sm-12 col-md-4 step">
+                    <div class="step-number"><span>Step</span><br/> 1</div>
+                    <h1>Download the App</h1>
+                    <p>Find us in the App Store. (Coming soon to Google Play!)</p>
+                </div>
+                <div class="col-sm-12 col-md-4 step">
+                    <div class="step-number"><span>Step</span><br/> 2</div>
+                    <h1>Pick your main &amp; side dishes</h1>
+                    <p>Bento’s dishes are prepared fresh & the options change everyday. </p>
+                </div>
+                <div class="col-sm-12 col-md-4 step">
+                    <div class="step-number"><span>Step</span><br/> 3</div>
+                    <h1>Enter your address. Done!</h1>
+                    <p>Delicious food will be delivered to your curbside in just minutes!</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="photos">
+        <div class="photo photo-large">
+            <img src="/site/img/photo-large-1.jpg" alt="Photo of Bento Box">
+        </div>
+        <div class="photo photo-large">
+            <img src="/site/img/photo-large-2.jpg" alt="Bento Mongolian Beef">
+        </div>
+        <div class="photo photo-small">
+            <img src="/site/img/photo-small-1.jpg" alt="Photo of Bento">
+        </div>
+        <div class="photo photo-small">
+            <img src="/site/img/photo-small-2.jpg" alt="Photo of Bento">
+        </div>
+        <div class="photo photo-small">
+            <img src="/site/img/photo-small-3.jpg" alt="Photo of Bento">
+        </div>
+        <div class="photo photo-small">
+            <img src="/site/img/photo-small-4.jpg" alt="Photo of Bento">
+        </div>
+    </section>
+
+    <footer class="footer">
+        <div class="footer-top">
+            <div class="container">
+                <div class="logo">
+                    <img src="/site/img/bento-logo.png" alt="Bento Logo" class="logo">
+                </div>
+                <div class="links">
+                    <a href="#">
+                        <img src="/site/img/app-store-button.png" alt="Download from App Store" class="app-button">
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <div class="container">
+                <div class="copyright">&copy; Bento</div>
+                <ul class="footer-nav">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Terms &amp; Conditions</a></li>
+                    <li><a href="#">Download Now</a></li>
+                    <li>415.300.1332</li>
+                    <li><a href="mailto:help@bentonow.com">help@bentonow.com</a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+
+
+    <script src="//bootstrapformhelpers.com/assets/js/jquery-1.10.2.min.js"></script>
+    <script src="/site/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="//d3q6uu7asevdsg.cloudfront.net/1.2/js/link_texting.min.js"></script>
+    <script src="/site/js/bootstrap-formhelpers.js"></script>
+    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&appId=488961274512887&version=v2.0";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+    </script>
+</body>
