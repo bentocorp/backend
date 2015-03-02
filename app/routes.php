eff8dc8600921be2f75377be442739fc1a2e4bab
Vincent Cardillo <vcardillo@gmail.com>
2015-03-02 11:51:50 -0800

    Adding site.

--- app/routes.php
+++ app/routes.php
@@ -13,8 +13,8 @@
 
 // Index
 Route::get('/', function() {
-    #return View::make('hello');
-    return Redirect::away('http://signup.bentonow.com');
+    return View::make('homepage.index');
+    #return Redirect::away('http://signup.bentonow.com');
 });
 
 // AWS Health check
