1357d01fcfab72b7e1697ecb9c2a9d7da22f5a57
Vincent Cardillo <vcardillo@gmail.com>
2015-03-13 17:33:23 -0700

    Asana 28957409094779: Show that we're on sale in the Admin! Otherwise we might forget.

--- app/views/admin/index.blade.php
+++ app/views/admin/index.blade.php
@@ -9,7 +9,7 @@
 
 <div class='alert alert-{{$statusClass}}' role='alert'><b>{{$statusMsg}}</b></div>
 
-<a href="/admin/status/open" onclick="return confirm('Open things up?')" class="btn btn-success">Open Us</a>
+<a href="/admin/status/open" onclick="return confirm('Open things up? Make sure we aren\'t on sale!')" class="btn btn-success">Open Us</a>
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 <a href="/admin/status/closed" onclick="return confirm('Shut it down, shut it all down?')" class="btn btn-danger">Shut it all down</a>
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
@@ -25,6 +25,7 @@ Edit some important copy on the dashboard
 <form method="post" action="/admin/misc/ioscopy" class="collapse" id="viewSomeCopy">
     <?php
     $price = 0;
+    $sale_price = 0;
     
     foreach ($iosCopy as $row) {
                 
@@ -44,8 +45,10 @@ Edit some important copy on the dashboard
             {{{$row->key}}}: <br>
             <input type="text" class="form-control" name="ioscopy[{{{$row->key}}}]" value="{{{$row->value}}}">
             <?php
-            if ($row->key == 'sale_price')
+            if ($row->key == 'sale_price') {
                 echo "<i>(normal price: $price)</i><br>";
+                $sale_price = $row->value;
+            }
             
             echo '<br>';
         }
@@ -54,6 +57,12 @@ Edit some important copy on the dashboard
             
     <button type="submit" class="btn btn-success">Save Copy</button>
 </form>
+
+<?php
+if ($price != $sale_price)
+    echo "<h3><span class='label label-warning'>We're on sale for $$sale_price!</span></h3>";
+?>
+
 <hr>
 
 
