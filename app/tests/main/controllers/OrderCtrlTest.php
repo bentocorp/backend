68ffb3402157df91191eeaab4a4bccc896b390b5
Vincent Cardillo <vcardillo@gmail.com>
2015-03-30 18:49:47 -0700

    Refactoring.

--- app/tests/main/controllers/OrderCtrlTest.php
+++ app/tests/main/controllers/OrderCtrlTest.php
@@ -104,59 +104,7 @@ class OrderCtrlTest extends TestCase {
         $this->assertResponseStatus(410);
     }
     
-    
-    public function testStripeOrderIntegrationWithUserCardSavedInDb() 
-    {
-        // Given an order from an authorized user who has a Stripe card on file with us,
-        $parameters = array(
-            "data" =>
-                '{
-                    "OrderItems": [
-                        {
-                            "item_type": "CustomerBentoBox",
-                            "items": [
-                                {"id": 1, "type": "main"},
-                                {"id": 2, "type": "side1"}
-                            ]
-                        }
-                    ],
-                    "OrderDetails": {
-                        "address": {
-                            "number": "1111",
-                            "street": "Kearny st.",
-                            "city": "San Francisco",
-                            "state": "CA",
-                            "zip": "94133"
-                        },
-                        "coords": {
-                            "lat": "37.798220",
-                            "long": "-122.405606"
-                        },
-                        "tax_cents": 137,
-                        "tip_cents": 200,
-                        "total_cents": "1537"
-                    },
-                    "Stripe": {
-                        "stripeToken": NULL
-                    }
-                }'
-                ,
-            "api_token" => "123"
-        );
-        
-        // and enough inventory for the order,
-        DB::table('LiveInventory')->truncate();
-        DB::insert('insert into LiveInventory (fk_item, qty) values (?, ?)', array(1, 100));
-        DB::insert('insert into LiveInventory (fk_item, qty) values (?, ?)', array(2, 100));
-                
-        // When I attempt to order
-        $response = $this->call('POST', '/order', $parameters);
-        
-        // Then I get ok
-        $this->assertResponseStatus(200);
-    }
-    
-        
+            
     public function testStripeOrderIntegrationFailsWithBadToken() 
     {
         // Given an order from an authorized user who is sending us a bad stripe token,
@@ -211,6 +159,58 @@ class OrderCtrlTest extends TestCase {
         $user = User::find(1);
         $user->stripe_customer_obj = NULL;
         $user->save();
+    }
+    
+    
+    public function testStripeOrderIntegrationWithUserCardSavedInDb() 
+    {
+        // Given an order from an authorized user who has a Stripe card on file with us,
+        $parameters = array(
+            "data" =>
+                '{
+                    "OrderItems": [
+                        {
+                            "item_type": "CustomerBentoBox",
+                            "items": [
+                                {"id": 1, "type": "main"},
+                                {"id": 2, "type": "side1"}
+                            ]
+                        }
+                    ],
+                    "OrderDetails": {
+                        "address": {
+                            "number": "1111",
+                            "street": "Kearny st.",
+                            "city": "San Francisco",
+                            "state": "CA",
+                            "zip": "94133"
+                        },
+                        "coords": {
+                            "lat": "37.798220",
+                            "long": "-122.405606"
+                        },
+                        "tax_cents": 137,
+                        "tip_cents": 200,
+                        "total_cents": "1537"
+                    },
+                    "Stripe": {
+                        "stripeToken": NULL
+                    }
+                }'
+                ,
+            "api_token" => "123"
+        );
+        
+        // and enough inventory for the order,
+        DB::table('LiveInventory')->truncate();
+        DB::insert('insert into LiveInventory (fk_item, qty) values (?, ?)', array(1, 100));
+        DB::insert('insert into LiveInventory (fk_item, qty) values (?, ?)', array(2, 100));
+                
+        // When I attempt to order
+        $response = $this->call('POST', '/order', $parameters);
+        
+        // Then I get ok
+        $this->assertResponseStatus(200);
     }
     
     
@@ -272,6 +272,58 @@ class OrderCtrlTest extends TestCase {
         
         // Clean up
         DB::update('update settings set `value` = "open" where `key` = ?', array('status'));
+    }
+    
+    
+    public function testCanOrderWithZeroAmount() 
+    {
+        // Given an order from an authorized user who has a Stripe card on file with us,
+        $parameters = array(
+            "data" =>
+                '{
+                    "OrderItems": [
+                        {
+                            "item_type": "CustomerBentoBox",
+                            "items": [
+                                {"id": 1, "type": "main"},
+                                {"id": 2, "type": "side1"}
+                            ]
+                        }
+                    ],
+                    "OrderDetails": {
+                        "address": {
+                            "number": "1111",
+                            "street": "Kearny st.",
+                            "city": "San Francisco",
+                            "state": "CA",
+                            "zip": "94133"
+                        },
+                        "coords": {
+                            "lat": "37.798220",
+                            "long": "-122.405606"
+                        },
+                        "tax_cents": 137,
+                        "tip_cents": 200,
+                        "total_cents": "000"
+                    },
+                    "Stripe": {
+                        "stripeToken": NULL
+                    }
+                }'
+                ,
+            "api_token" => "123"
+        );
+        
+        // and enough inventory for the order,
+        DB::table('LiveInventory')->truncate();
+        DB::insert('insert into LiveInventory (fk_item, qty) values (?, ?)', array(1, 100));
+        DB::insert('insert into LiveInventory (fk_item, qty) values (?, ?)', array(2, 100));
+                
+        // When I attempt to order
+        $response = $this->call('POST', '/order', $parameters);
+        
+        // Then I get ok
+        $this->assertResponseStatus(200);
     }
 
     
