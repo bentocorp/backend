76d0c1b9ea54aef6e4a6e024e1303b978cf8df10
Vincent Cardillo <vcardillo@gmail.com>
2016-02-24 02:29:19 +0000

    All personal User coupons, are now only good for your first order.

--- app/views/emails/transactional/order_confirmation.blade.php
+++ app/views/emails/transactional/order_confirmation.blade.php
@@ -151,8 +151,9 @@ else if ($order->order_type == 2)
 				<!-- block -->
 				<tr>
 					<td mc:edit="block-20" class="textblock-01" style="padding: 25px 40px 27px; font: 18px/24px Arial, Verdana, Helvetica, sans-serif; color: #fff;" align="center" bgcolor="#8abb5a">
-						Give your friends $5 off their first Bento order when they use your promo code: <b>{{$user->coupon_code}}</b>.<br>
-						(It works once for you too.)
+                                            Give your friends $5 off their first Bento order when they use your promo code: <br>
+                                            <b>{{$user->coupon_code}}</b><br>
+                                            (It works once for you too.)
 					</td>
 				</tr>
 				<!-- shadow-blocks -->
